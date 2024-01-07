01409b7519e58c165b9ae6925a6fcd0f73523019
unknown <mahmoudelsawy057@gmail.com>
2024-01-07 14:37:18 +0400

    adding register form validation

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,15 +1,51 @@
 import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
+import { useForm, SubmitHandler } from "react-hook-form";
+import InputErrorMessage from "../components/InputErrorMsg";
+import { REGISTER_FORM } from "../data";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { validationSchema } from "../validation";
+
+interface IFormInput {
+  username: string;
+  email: string;
+  password: string;
+}
 
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({ resolver: yupResolver(validationSchema) });
+
+  // Handlers
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  // Renders
+
+  const renderRegisterForm = REGISTER_FORM.map(
+    ({ name, type, placeholder, validation }, idx) => (
+      <div key={idx}>
+        <Input
+          type={type}
+          placeholder={placeholder}
+          {...register(name, validation)}
+        />
+        {errors[name] && <InputErrorMessage msg={errors[name]?.message} />}
+      </div>
+    )
+  );
+
+  console.log(errors);
+
   return (
     <div className="max-w-md mx-auto">
-      <h2 className="text-center mb-4 text-3xl font-semibold">Register to get access!</h2>
-      <form className="space-y-4">
-        <Input placeholder="Username" />
-        <Input placeholder="Email address" />
-        <Input placeholder="Password" />
-
+      <h2 className="text-center mb-4 text-3xl font-semibold">
+        Register to get access!
+      </h2>
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        {renderRegisterForm}
         <Button fullWidth>Register</Button>
       </form>
     </div>
---
+++ src/validation/index.ts
@@ -0,0 +1,18 @@
+import * as yup from "yup";
+
+export const validationSchema = yup
+  .object({
+    username: yup
+      .string()
+      .required("username is required")
+      .min(5, "username should be atleast 5 characters"),
+    email: yup
+      .string()
+      .required("email is required")
+      .matches(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/, "not a valid email"),
+    password: yup
+      .string()
+      .required("password is required")
+      .min(6, "password should be atleast 5 characters"),
+  })
+  .required();
